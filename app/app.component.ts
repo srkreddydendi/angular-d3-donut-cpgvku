1086875f4e6692d6f5bd036eab26b6fe8bd76120
srkreddydendi <srkreddydendi@gmail.com>
2020-04-09 17:19:04 +0530

    3 files modified

--- app/app.component.ts
+++ app/app.component.ts
@@ -9,7 +9,7 @@ export class AppComponent  {
   name = 'Angular 5';
   public donutChartData = [{
         id: 0, // number
-        label: '',  // string
+       // label: '',  // string
         value: 70,  // number
         color: '#ccc',  // string,
       //  iconImage: 'https://avatars.discourse.org/v2/letter/a/a8b319/45.png' // string
@@ -17,7 +17,7 @@ export class AppComponent  {
         id: 1, // number
         label: '',  // string
         value: 30,  // number
-        color: '#DA4F49',  // string,
+        color: 'blue',  // string,
       //  iconImage: 'https://avatars.discourse.org/v2/letter/a/a8b319/45.png' // string
     }
 ]
